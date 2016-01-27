1b64c668a90d569f68dc8f7c165b720f4005f229
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2016-01-27 16:50:29 +0000

    Back after a coffee and a break and Elpiniki Iosif got to use ebay.com.au for a bit\!

--- XoopsModules/fontier+/trunk/modules/fontier/header.php
+++ XoopsModules/fontier+/trunk/modules/fontier/header.php
@@ -1,24 +1,25 @@
 <?php
 /**
- * Chronolabs Fonting Repository Services REST FONTIER FONTIER
-*
-* You may not change or alter any portion of this comment or credits
-* of supporting developers from this source code or any supporting source code
-* which is considered copyrighted (c) material of the original comment or credit authors.
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-*
-* @copyright       Chronolabs Cooperative http://labs.coop
-* @license         General Public License version 3 (http://labs.coop/briefs/legal/general-public-licence/13,3.html)
-* @package         fonts
-* @since           2.1.9
-* @author          Simon Roberts <wishcraft@users.sourceforge.net>
-* @subpackage		FONTIER
-* @description		Fonting Repository Services REST FONTIER
-* @link			http://sourceforge.net/projects/chronolabsFONTIERs
-* @link			http://cipher.labs.coop
-*/
+ * Chronolabs Cooperative Fontier+
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       Chronolabs Cooperative http://labs.coop
+ * @license         General Public License version 3 (http://labs.coop/briefs/legal/general-public-licence/13,3.html)
+ * @package         fontier+
+ * @since           1.0.1
+ * @author          Simon Roberts <wishcraft@users.sourceforge.net>
+ * @subpackage		fontier+
+ * @description		XOOPS Client for Fonts REST API - http://fonts.labs.coop
+ * @link			http://fonts.labs.coop
+ * @link			http://sourceforge.net/projects/chronolabs
+ * @link			http://cipher.labs.coop
+ */
 	
 	error_reporting(E_ERROR);
 	ini_set('display_errors', true);
