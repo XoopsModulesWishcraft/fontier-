44abab3b2d843446d96cca390757ee002a718c8b
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2016-01-27 16:50:29 +0000

    Back after a coffee and a break and Elpiniki Iosif got to use ebay.com.au for a bit\!

--- modules/fontier/callback.php
+++ modules/fontier/callback.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Chronolabs Entitiesing Repository Services REST API API
+ * Chronolabs Cooperative Fontier+
  *
  * You may not change or alter any portion of this comment or credits
  * of supporting developers from this source code or any supporting source code
@@ -11,12 +11,13 @@
  *
  * @copyright       Chronolabs Cooperative http://labs.coop
  * @license         General Public License version 3 (http://labs.coop/briefs/legal/general-public-licence/13,3.html)
- * @package         entities
- * @since           2.1.9
+ * @package         fontier+
+ * @since           1.0.1
  * @author          Simon Roberts <wishcraft@users.sourceforge.net>
- * @subpackage		api
- * @description		Entitiesing Repository Services REST API
- * @link			http://sourceforge.net/projects/chronolabsapis
+ * @subpackage		fontier+
+ * @description		XOOPS Client for Fonts REST API - http://fonts.labs.coop
+ * @link			http://fonts.labs.coop
+ * @link			http://sourceforge.net/projects/chronolabs
  * @link			http://cipher.labs.coop
  */
 
